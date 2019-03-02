9e7b6a6288b4b79f9ba54dbd692cf780152a48aa
SHADAI ALI <nafiouali@gmail.com>
2019-03-02 20:30:30 +0100

    update

--- assets/js/invoke.js
+++ assets/js/invoke.js
@@ -1,23 +1,10 @@
-window.addEventListener("load",function () {
+window.addEventListener("load", function () {
     const button = document.querySelector("#kkiapay-button");
-    console.debug("inputs",inputs);
+    console.debug("inputs", inputs);
 
-    button.addEventListener('click',function (event) {
+    button.addEventListener('click', function (event) {
         event.preventDefault();
+        inputs.sdk = "woocommerce"
         window.openKkiapayWidget(inputs)
     });
 });
-
-/*
-{
-            key : inputs.api_key,
-            amount : inputs.amount,
-            theme: inputs.theme,
-            url: inputs.url,
-            name : inputs.name,
-            lang: inputs.lang,
-            phone: inputs.phone,
-            position: inputs.position,
-            callback: inputs.callback
-        }
- */
--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -27,7 +27,7 @@ cd ../
 svn cp trunk tags/$TRAVIS_TAG
 
 # 8. Push SVN tag
-svn ci --message "Release $TRAVIS_TAG" \
+svn ci --trust-server-cert --non-interactive --message "Release $TRAVIS_TAG" \
     --username $SVN_USERNAME \
     --password $SVN_PASSWORD \
     --non-interactive
