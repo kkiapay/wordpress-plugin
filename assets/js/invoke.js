7cf8e9491f37bc90872016b884a850d8bbc9cce5
lephenix1234 <38128001+lephenix1234@users.noreply.github.com>
2019-05-29 13:08:42 +0100

    sandbox
    add params for sandbox

--- assets/js/invoke.js
+++ assets/js/invoke.js
@@ -1,9 +1,7 @@
 window.addEventListener("load", function () {
     const button = document.querySelector("#kkiapay-button");
     console.debug("inputs", inputs);
-    if(inputs.testmode === "yes") {
-        inputs.test = "sandbox"
-    }
+    inputs.testmode === "yes" ? inputs.test = "sandbox" : ""
 
     button.addEventListener('click', function (event) {
         event.preventDefault();
