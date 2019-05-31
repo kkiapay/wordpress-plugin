5f533d8941311af52ad30218abb11de15513d752
shadai.ali <nafiouali@gmail.com>
2019-05-31 11:43:37 +0100

    update ci

---
+++ build-cfg/utils/config.php
@@ -0,0 +1,6 @@
+<?php
+// $root_dir = dirname( $parent_dir );
+// $src_dir = $root_dir . '/src/wp-migrate-db-pro-cli';
+// $tmp_dir = $root_dir . '/builds';
+// $zip_dir = $tmp_dir;
+// $zip_name = 'my-awesome-plugin';
