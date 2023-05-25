c8b36f339c0ca972e844fe87b68080694846878b
salem.affa <salem.affa@mehriso.com>
2023-05-25 11:57:01 +0100

    feat: update version

--- kkiapay-woocommerce-plugin.php
+++ kkiapay-woocommerce-plugin.php
@@ -6,17 +6,18 @@
  * Author: Kkiapay Developer Team ❤️
  * Author URI: https://kkiapay.me/
  * License: GPLv2
- * Version: 2.3.16
+ * Version: 2.3.17
  * Requires at least: 4.4
  * Tested up to: 5.7.2
  * WC requires at least: 2.6
  * WC tested up to: 5.0.0
  * Text Domain: kkiapay-woocommerce
  * Domain Path: /languages
-*/
+ */
 
 // Make sure WooCommerce is active
- if ( ! in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) return;
+if (!in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_option('active_plugins'))))
+    return;
 
 // include kkiapay
 require_once(plugin_dir_path(__DIR__) . 'kkiapay-woocommerce/vendor/autoload.php');
@@ -24,44 +25,48 @@ require_once(plugin_dir_path(__DIR__) . 'kkiapay-woocommerce/vendor/autoload.php
 /*
  * The class itself, please note that it is inside plugins_loaded action hook
  */
-function kkiapay_init_gateway_class() {
-	if ( ! class_exists( 'WC_Payment_Gateway' ) ) return;
+function kkiapay_init_gateway_class()
+{
+    if (!class_exists('WC_Payment_Gateway'))
+        return;
     require_once(plugin_dir_path(__FILE__) . 'includes/class-wc-kkiapay-gateway.php');
-    
-    add_filter( 'woocommerce_payment_gateways', 'kkiapay_add_gateway_class' );
+
+    add_filter('woocommerce_payment_gateways', 'kkiapay_add_gateway_class');
 
     /*
-    * This action hook registers our PHP class as a WooCommerce payment gateway
-    */
-        function kkiapay_add_gateway_class( $gateways ) {
-	        $gateways[] = 'WC_Kkiapay_Gateway';
-	    return $gateways;
-        } 
+     * This action hook registers our PHP class as a WooCommerce payment gateway
+     */
+    function kkiapay_add_gateway_class($gateways)
+    {
+        $gateways[] = 'WC_Kkiapay_Gateway';
+        return $gateways;
+    }
 
 }
 
-add_action( 'plugins_loaded', 'kkiapay_init_gateway_class' );
+add_action('plugins_loaded', 'kkiapay_init_gateway_class');
 
 /**
  * Adds plugin action links.
  */
-function kkiapay_action_links( $links ) {
+function kkiapay_action_links($links)
+{
 
-	$links[] = 	'<a href="admin.php?page=wc-settings&tab=checkout&section=kkiapay_woocommerce_plugin">' . __('Settings', 'kkiapay-woocommerce') . '</a>';
-	$links[] = '<a href="https://docs.kkiapay.me/v1/plugin-et-sdk/woo-commerce" target="_blank">' . __('Docs', 'kkiapay-woocommerce') . '</a>';
-	$links[] = '<a href="http://support.kkiapay.me" target="_blank">' .__('Support', 'kkiapay-woocommerce'). '</a>';
+    $links[] = '<a href="admin.php?page=wc-settings&tab=checkout&section=kkiapay_woocommerce_plugin">' . __('Settings', 'kkiapay-woocommerce') . '</a>';
+    $links[] = '<a href="https://docs.kkiapay.me/v1/plugin-et-sdk/woo-commerce" target="_blank">' . __('Docs', 'kkiapay-woocommerce') . '</a>';
+    $links[] = '<a href="http://support.kkiapay.me" target="_blank">' . __('Support', 'kkiapay-woocommerce') . '</a>';
 
-	return $links;
+    return $links;
 }
 
-add_action( 'plugin_action_links_' . plugin_basename( __FILE__ ), 'kkiapay_action_links' );
+add_action('plugin_action_links_' . plugin_basename(__FILE__), 'kkiapay_action_links');
 
 /**
  * Load tanslation files
  */
-function kkiapay_load_plugin_textdomain() {
-    load_plugin_textdomain( 'kkiapay-woocommerce', FALSE, basename( dirname( __FILE__ ) ) . '/languages/' );
+function kkiapay_load_plugin_textdomain()
+{
+    load_plugin_textdomain('kkiapay-woocommerce', FALSE, basename(dirname(__FILE__)) . '/languages/');
 }
 
-add_action( 'plugins_loaded', 'kkiapay_load_plugin_textdomain' );
-
+add_action('plugins_loaded', 'kkiapay_load_plugin_textdomain');
