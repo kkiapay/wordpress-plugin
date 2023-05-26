90951f898f1af785aebbe5d7a56ca7fff00a09ed
Salem AFFA <50690169+asaje379@users.noreply.github.com>
2023-05-26 15:43:27 +0100

    Update deploy.sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -87,7 +87,8 @@ svn stat svn | grep '^!' | awk '{print $2}' | xargs -I x svn rm --force x@
 svn stat svn
 
 # Commit to SVN
-svn ci --no-auth-cache --username $WP_ORG_USERNAME --password $SVN_PASSWORD svn -m "Deploy version $VERSION"
+svn ci --no-auth-cache --username $WP_ORG_USERNAME --password $SVN_PASSWORD 
+svn -m "Deploy version $VERSION"
 
 # Remove SVN temp dir
 rm -fR svn
